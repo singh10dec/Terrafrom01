ca4351255925a2dc8d16779c0307e0e3fbb167c5
singh07 <balwantpsingh@hotmail.com>
2019-09-23 09:28:07 +0000

    Added File

--- main.ft.tf
+++ main.ft.tf
@@ -1,8 +1,17 @@
-resource "azure_resource_group" "test" {
-    name ="testResourceGroup1"
-    location = "southIndia"
+#Create Resource Group
+resource "azurerm_resource_group" "test" {
+  name     = "testResourceGroup1"
+  location = "West US"
 
-    tags{
-    environment ="Production"
-    } 
+  tags = {
+    environment = "Production"
+  }
+}
+#Create Virtual net work
+resource  "azurerm_virtual_network" "vnet"{
+
+ name ="MyTFNet"
+ address_space = ["10.0.0.0/16"]
+ location  = "westus2"
+ azurerm_resource_group = "${azurerm_resource_group.testResourceGroup1.name}"
 }
